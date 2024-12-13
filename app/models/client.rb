1d243335a866f54bfa1fddf2dbe09cf164234dea
Odell McFarland IV <odell.mac4@gmail.com>
2024-12-12 18:55:19 -0800

    Insert SQL queries within model methods for efficient testing in rails console

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,25 @@ class Client < ApplicationRecord
   has_many :client_providers
   has_many :providers, through: :client_providers
   has_many :journal_entries
+
+  def find_providers
+    sql = <<-SQL
+      SELECT providers.*
+      FROM providers
+      INNER JOIN client_providers cp on providers.id = cp.provider_id
+      WHERE cp.client_id = ?
+    SQL
+    Provider.find_by_sql([sql, self.id])
+  end
+
+  def find_journal_entries
+    sql = <<-SQL
+      SELECT je.*
+      FROM journal_entries je
+      INNER JOIN clients c ON je.client_id = c.id
+      WHERE c.id = ?
+      ORDER BY je.created_at;
+    SQL
+    JournalEntry.find_by_sql([sql, self.id])
+  end
 end
--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,4 +1,28 @@
 class Provider < ApplicationRecord
   has_many :client_providers
   has_many :clients, through: :client_providers
+
+  def find_clients
+    sql = <<-SQL
+      SELECT clients.*
+      FROM clients
+      INNER JOIN client_providers cp ON clients.id = cp.client_id
+      WHERE cp.provider_id = ?
+    SQL
+    Client.find_by_sql([sql, self.id])
+  end
+
+  def find_clients_journal_entries
+    sql = <<-SQL
+      SELECT
+        je.*
+      FROM
+        journal_entries je
+      INNER JOIN clients c ON je.client_id = c.id
+      INNER JOIN client_providers cp ON c.id = cp.client_id
+      WHERE cp.provider_id = ?
+      ORDER BY je.created_at;
+    SQL
+    JournalEntry.find_by_sql([sql, self.id])
+  end
 end
